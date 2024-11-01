86d90d4df0d1631138e5192b87672c42542a7080
Haruto Kasuga <haruto@Harutononotobukkukonpyuta.local>
2024-11-01 13:08:36 +0900

    feat: login function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: 'ユーザーが作成されました'
+      session[:user_id] = @user.id # 作成したユーザーでログイン
+      redirect_to home_path, notice: 'ユーザーが作成されました' # home_pathに遷移
     else
       render :new
     end
